2f7ddaec53d5027c48cb999f51ed1d4003c17e35
Saurav Singh <sauravsingh7@outlook.com>
2017-03-21 20:51:17 +0530

    take input from user

--- src/com/ss/anagram/CheckAnagram.java
+++ src/com/ss/anagram/CheckAnagram.java
@@ -4,6 +4,7 @@
 package com.ss.anagram;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * @author Saurav Singh
@@ -12,7 +13,22 @@ import java.util.Arrays;
 public class CheckAnagram {
 
 	public static void main(String[] args) {
-		System.out.println(isAnagram("saurav", "ravsau"));
+		String inpOne;
+		String inpTwo;
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(System.in);
+			System.out.print("Please enter the first string :: ");
+			System.out.println("");
+			inpOne = scanner.next();
+			System.out.print("Please enter the two string :: ");
+			System.out.println("");
+			inpTwo = scanner.next();
+		}
+		finally {
+			scanner.close();
+		}
+		System.out.println(isAnagram(inpOne, inpTwo));
 	}
 
 	static boolean isAnagram(final String inpOne, final String inpTwo) {
